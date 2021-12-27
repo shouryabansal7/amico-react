9397326b74b6e32c172024d85a9d79145ffe2a2f
Shourya Bansal <shouryab2001@gmail.com>
2021-12-27 13:46:24 +0530

    sixteenth commit : add post functionality

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,5 +1,6 @@
-import { UPDATE_POSTS } from './actioneTypes';
+import { ADD_POST, UPDATE_POSTS } from './actioneTypes';
 import { APIurls } from '../helper/urls';
+import { getAuthTokenFromLocalStorage, getFormBody } from '../helper/utils';
 
 export function fetchPosts() {
   return (dispatch) => {
@@ -20,5 +21,35 @@ export function updatePosts(posts) {
   return {
     type: UPDATE_POSTS,
     posts,
+  };
+}
+
+export function addPost(post) {
+  return {
+    type: ADD_POST,
+    post,
+  };
+}
+
+export function createPost(content) {
+  return (dispatch) => {
+    const url = APIurls.createPost();
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+      body: getFormBody({ content }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('dATA', data);
+
+        if (data.success) {
+          dispatch(addPost(data.data.post));
+        }
+      });
   };
 }
--- src/helper/urls.js
+++ src/helper/urls.js
@@ -11,4 +11,5 @@ export const APIurls = {
     `${API_ROOT}/friendship/create_friendship?user_id=${userId}`,
   removeFriends: (userId) =>
     `${API_ROOT}/friendship/remove_friendship?user_id=${userId}`,
+  createPost: () => `${API_ROOT}/posts/create`,
 };
